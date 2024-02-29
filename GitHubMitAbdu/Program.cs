26c30bb44d405815eeb4777f05f22876a8d2e762
Erol <ebilyalov@bhakwien10.at>
2024-02-29 10:54:43 +0100

    Add project files.

---
+++ GitHubMitAbdu/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitHubMitAbdu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
